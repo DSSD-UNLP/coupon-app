68669d624294876d176adf120cda8046adb2759f
Julian Pasquale <julian.pasquale@snappler.com>
2018-10-13 20:30:53 -0300

    procfile changed

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn couponProject.wsgi
+web: python manage.py runserver
--- couponProject/couponApp/urls.py
+++ couponProject/couponApp/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^coupons$', views.CouponList.as_view()),
-    url(r'^coupons/(?P<name>.*)$', views.CouponDetail.as_view()),
+    url(r'^coupons/$', views.CouponList.as_view()),
+    url(r'^coupons/(?P<name>.*)/$', views.CouponDetail.as_view()),
 ]
