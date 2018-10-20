19a31c36a13c81ce1b8331743844113e37802cf9
Julian Pasquale <julian.pasquale@snappler.com>
2018-10-20 19:08:47 -0300

    refactoring views, model validations and serializer

--- couponProject/couponApp/urls.py
+++ couponProject/couponApp/urls.py
@@ -4,6 +4,6 @@ from django.conf             import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^coupons/$', views.CouponList.as_view()),
+    url(r'^coupons/$',              views.CouponCreate.as_view()),
     url(r'^coupons/(?P<name>.*)/$', views.CouponDetail.as_view()),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
